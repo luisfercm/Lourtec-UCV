b32b64f2d78762c09f25620d7e2fff5c8ad6bc6f
luisfercm <luis15774@hotmail.com>
2017-08-26 16:58:55 -0500

    Estructuras, Enumerados

---
+++ Dia 2/StructDemoVS/StructDemoVS/BillingDetails.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDemoVS
+{
+    
+    public struct BillingDetails
+    {
+        //public BillingDetails(string name, int myVar , string publicField, string accountNumber,int publicVar  )
+        //{
+        //    this.name = name;
+        //    this.myVar = myVar;
+        //    this.publicField = publicField;
+        //    this.AccountNumber = accountNumber;
+            
+        //}
+
+        //atajo "ctor"
+
+        //public BillingDetails()
+        //{
+
+        //}
+
+        private string name;
+
+        public string publicField;
+
+        // Forma explicita
+        public string Name
+        {
+            get {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        // Forma implicita
+        public string AccountNumber
+        {
+            get;
+            set;
+        }
+
+        // escribir prop + tab
+        //public int MyProperty { get; set; }
+
+        public int MyPropertyProp { get; set; }
+
+
+        // escribir propa + tab
+
+
+
+
+        // escribir propfull + tab
+
+        private int myVar;
+
+        public int MyPropertyFull
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+        // escribir propg+ tab
+
+        public int MyPropertyG { get; private set; }
+
+
+
+
+    }
+}
---
+++ Dia 2/StructDemoVS/StructDemoVS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDemoVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BillingDetails billing = new BillingDetails();
+            billing.AccountNumber = "1234567890";
+            billing.Name = "Moises Chirinos";
+
+            Console.WriteLine("Nombre de cliente:"+ billing.Name);
+            Console.WriteLine($"Numero de cuenta cliente:{ billing.AccountNumber}");
+
+            Console.ReadKey();
+        }
+    }
+}
