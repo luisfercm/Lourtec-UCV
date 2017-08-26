c827e676f6e61fdea4622870929f281016b0cacb
luisfercm <luis15774@hotmail.com>
2017-08-26 18:03:52 -0500

    Fuentes  de Datos y Estructuras

--- Dia 2/BusTicket/BusTicket/MainWindow.xaml.cs
+++ Dia 2/BusTicket/BusTicket/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusTicket.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,67 @@ namespace BusTicket
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Reserva Reserva { get; set; }
+        public List<Rutas> Rutas { get; set; }
+        public List<Rutas> Resultados { get; set; }
+
+        public List<Localizacion> Estaciones { get; set; }
+
         public MainWindow()
         {
+            Estaciones = new List<Localizacion>()
+            {
+                new Localizacion
+                {
+                    Ciudad ="Lima",
+                    Estacion ="Lima Central",
+                    Estado = "Lima",
+                    Pais="Peru"
+                },
+                new Localizacion
+                {
+                    Ciudad ="Trujillo",
+                    Estacion ="Trujillo Central",
+                    Estado = "Trujillo",
+                    Pais="Peru"
+                }
+            };
+
+            Reserva = new Reserva()
+            {
+                Destino = Estaciones[0],
+                Salida = Estaciones[1],
+                Fecha = DateTime.Now
+            };
+
+            Rutas = new List<Models.Rutas>() {
+                 new Rutas(){
+                      Chofer="MC",
+                      Compania="L",
+                      Estaciones = Estaciones,
+                      FechaFin = DateTime.Now.AddDays(2),
+                      FechaInicio = DateTime.Now,
+                      Id= Guid.NewGuid()
+                 },
+                  new Rutas(){
+                      Chofer="XYZ",
+                      Compania="Z",
+                      Estaciones = Estaciones,
+                      FechaFin = DateTime.Now.AddDays(12),
+                      FechaInicio = DateTime.Now.AddDays(10),
+                      Id= Guid.NewGuid()
+                 }
+            };
+
             InitializeComponent();
-        }
+
+            //this.Origen.Items.Add(Estaciones);
+            this.Origen.ItemsSource = Estaciones;
+            this.Origen.SelectedItem = Reserva.Salida;
+            this.Destino.ItemsSource = Estaciones;
+            //this.Destino.Items.Add(Estaciones);
+            this.Destino.SelectedItem = Reserva.Destino;
+            this.DiaViaje.SelectedDate = Reserva.Fecha;
+        }   
     }
 }
--- Dia 2/BusTicket/BusTicket/Models/Localizacion.cs
+++ Dia 2/BusTicket/BusTicket/Models/Localizacion.cs
@@ -12,5 +12,11 @@ namespace BusTicket.Models
         public string Ciudad { get; set; }
         public string Pais { get; set; }
         public string Estacion { get; set; }
+
+        //Sobrecargamos el metodo
+        public override string ToString()
+        {
+            return $"{Estacion}-{Ciudad},{Estado},{Pais}";
+        }
     }
 }
