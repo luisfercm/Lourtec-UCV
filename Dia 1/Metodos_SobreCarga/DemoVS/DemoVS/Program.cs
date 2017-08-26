e7cfb89451aca6f13818e5b34a572961e3014df9
luisfercm <luis15774@hotmail.com>
2017-08-26 16:54:58 -0500

    primer día, tipo de datos, metodos, sobrecarga

---
+++ Dia 1/Metodos_SobreCarga/DemoVS/DemoVS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            var programa = new Program();
+
+            int num=0;
+            var numero = programa.GetNumero(num);
+            Execute();
+            
+            Console.ReadLine();
+        }
+
+        int GetNumero(out int nombre) {
+            nombre = 10;
+            return 5;
+        }
+
+        int GetNumero(int a , int b=0)
+        {
+            return a + b ;
+        }
+        static void Execute() {
+
+        }
+    }
+}
