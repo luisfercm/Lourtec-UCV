b08d8abc5f477fccbfb3761e7e14c305a17a9c96
luisfercm <luis15774@hotmail.com>
2017-09-01 20:00:13 -0500

    Interfaces

---
+++ Dia 3/Classes, Interfaces/DemoInterfaces/DemoInterfaces/GenericDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoInterfaces
+{
+    class GenericDemo:IEnumerable<Gato>
+    {
+
+        List<int> listEnteros;
+        List<string> listString;
+        List<Perro> listPerro;
+        Queue<Lobo> listLobo;
+        Dictionary<Guid, Paloma> diccionarioRatas;
+
+        public IEnumerator<Gato> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
