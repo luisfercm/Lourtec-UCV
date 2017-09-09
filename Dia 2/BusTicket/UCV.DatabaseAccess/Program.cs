3ab8dedb84b76493248d400dbedc6f43893a4604
luisfercm <luis15774@hotmaill.com>
2017-09-08 22:11:10 -0500

    avances

--- Dia 2/BusTicket/UCV.DatabaseAccess/Program.cs
+++ Dia 2/BusTicket/UCV.DatabaseAccess/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UCV.Comun.Interfaces;
+using UCV.Comun.Modelos;
+using UCV.DatabaseAccess.Contextos;
+using UCV.DatabaseAccess.Servicios;
 
 namespace UCV.DatabaseAccess
 {
@@ -10,7 +14,58 @@ namespace UCV.DatabaseAccess
     {
         static void Main(string[] args)
         {
+            //var servicioCompania = new ServicioCompania();
 
+            IServicioCompania servicioCompania = new ServicioCompania();
+
+
+            var compania = new Compania()
+            {
+                Ruc = $"Ruc Unico { new Random().Next(1, 1000000).ToString()}",
+                Calificacion = new Random().Next(1, 10)
+            };
+
+
+
+            var companias = new List<Compania>() {
+                new Compania() {
+                Ruc= $"Ruc Unico { new Random().Next(1,1000000).ToString()}",
+                Calificacion = new Random().Next(1,10)
+            },
+                null,
+              new Compania() {
+                Ruc= String.Empty,
+                Calificacion = 1
+            } };
+
+            try
+            {
+                servicioCompania.SaveCompania(compania);
+                servicioCompania.SaveCompania(companias);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No se pudo guardar porque: " + e.Message);
+            }
+            
+
+
+            foreach (var c in servicioCompania.GetCompanias())
+            {
+                Console.WriteLine($"-- Registro --");
+                Console.WriteLine($"{c.Id}-{c.Ruc}-{c.Calificacion}");
+
+            }
+
+
+
+            while (true)
+            {
+                if (Console.ReadLine() == "quit")
+                {
+                    break;
+                }
+            }
 
         }
     }
--- Dia 2/BusTicket/UCV.DatabaseAccess/Servicios/ServicioCompania.cs
+++ Dia 2/BusTicket/UCV.DatabaseAccess/Servicios/ServicioCompania.cs
@@ -24,7 +24,7 @@ namespace UCV.DatabaseAccess.Servicios
 
         public List<Compania> GetCompanias()
         {
-            throw new NotImplementedException();
+            return Db.Companias.ToList();
         }
 
         public void SaveCompania(Compania compania)
@@ -32,16 +32,49 @@ namespace UCV.DatabaseAccess.Servicios
             if (compania.Ruc == null || compania.Ruc== string.Empty) {
                 throw new NullReferenceException("El Ruc no puede ser nulo o vacio");
             }
-
             
             compania.Id = Guid.NewGuid();
             Db.Companias.Add(compania);
             Db.SaveChanges();
         }
 
-        public void SaveCompania(List<Compania> compania)
+        public void SaveCompania(List<Compania> companias)
         {
-            throw new NotImplementedException();
+            // Validar si hay elementos vacios en la coleccion Linq Funcional/Extension
+            var hayElementosVacios = companias.Where(g => g == null ||
+                                   g.Ruc == null ||
+                                   g.Ruc == String.Empty)
+                            .Count() > 0;
+
+            // Validar si hay elementos vacios en la coleccion Linq Clasico
+            hayElementosVacios = (from g in companias
+                                  where g == null ||
+                                        g.Ruc == null ||
+                                        g.Ruc == String.Empty
+                                  select g).Count() > 0;
+            if (hayElementosVacios)
+            {
+                throw new NullReferenceException("El Ruc no puede ser nulo o vacio");
+            }
+            // Recrear todos los valores con un ID nuevo Linq Funcional/Extension
+            var data = companias.Select(g => new Compania
+            {
+                Id = Guid.NewGuid(),
+                Ruc = g.Ruc,
+                Calificacion = g.Calificacion
+            });
+            // Recrear todos los valores con un ID nuevo Linq Clasico
+            data = from g in companias
+                   select new Compania()
+                   {
+                       Id = Guid.NewGuid(),
+                       Ruc = g.Ruc,
+                       Calificacion = g.Calificacion,
+                   };
+
+            Db.Companias.AddRange(data);
+            Db.SaveChanges();
+
         }
 
         public void UpdateCompania(Compania compania)
